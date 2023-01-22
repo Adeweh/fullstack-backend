10616c858359cb299419e7928ff4ea581572eb2d
Adeweh <dorcasabang@gmail.com>
2023-01-22 18:30:08 +0100

    Exception handling

--- src/main/java/com/springboot/fullstackbackend/controller/UserController.java
+++ src/main/java/com/springboot/fullstackbackend/controller/UserController.java
@@ -1,20 +1,18 @@
 package com.springboot.fullstackbackend.controller;
 
+import com.springboot.fullstackbackend.exception.UserNotFoundException;
 import com.springboot.fullstackbackend.model.User;
 import com.springboot.fullstackbackend.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class UserController {
-
     @Autowired
     private UserRepository userRepository;
 
@@ -28,6 +26,27 @@ public class UserController {
 
         return ResponseEntity.ok(userRepository.findAll());
     }
+
+    @GetMapping("user/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id)));
+    }
+
+    @PutMapping("user/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.findById(id)
+                        .map(user -> {
+                            user.setUsername(newUser.getUsername());
+                            user.setFirstname(newUser.getFirstname());
+                            user.setLastname(newUser.getLastname());
+                            user.setEmail(newUser.getEmail());
+
+                            return userRepository.save(user);
+                        })
+                .orElseThrow(() -> new UserNotFoundException(id)));
+    }
+
 
 
 
